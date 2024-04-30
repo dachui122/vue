04b01966a670de0cfe60cfdc3e9df0b3537753aa
dachui122 <1183725698@qq.com>
2024-04-30 09:59:32 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app">
+        <h1>你好啊</h1>
+        <Person/>
+</div>
+</template>
+
+
+<script lang="ts">
+import Person from './components/Person.vue'
+
+export default {
+    name: 'App',
+    components: {Person}//组件名
+}
+</script>
+<style >
+.app {
+    background-color: #bf0808;
+    box-shadow: 0 0 10px;
+    border-radius:10px;
+    padding:20px;
+    margin:10px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="person">
+    <h2>当前求和为{{ sum }}</h2>
+    <button @click="changeSum">点我sum加一</button>
+  </div>
+</template>
+<script lang="ts" setup name="Person">
+import { ref, watch } from 'vue'
+
+//数据
+let sum = ref(0)
+//方法
+function changeSum() {
+  sum.value +=1
+}
+//监视
+
+watch(sum,(_newvalue,_oldvalue)=>{
+  console.log('sum变化了')
+})
+</script>
+
+
+
+
+
+<style  scoped>
+li{
+  padding: 10px;
+  margin: 0 5px;
+}
+.person{
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding:10px;
+}
+button{
+  padding: 10px;
+  margin: 0 5px;
+  
+}
+</style>
